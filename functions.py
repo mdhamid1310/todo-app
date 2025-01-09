f24afbbf1a9f2c54d6c2f8b875b91211c0e8a912
mdhamid1310 <john@example.com>
2025-01-09 17:54:47 +0530

    Add button and working od add button

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
+FILEPATH = "files/todos.txt"
 def get_todos():
-    with open('files/todos.txt', 'r') as file_local:
+    with open(FILEPATH, 'r') as file_local:
         todos_local = file_local.readlines()
         # file=open('files/todos.txt', 'r')
         # todos=file.readlines()
@@ -7,7 +8,7 @@ def get_todos():
         # alternate file handling method is with context manager
     return todos_local
 
-def write_todos(filepath,todos_arg):
+def write_todos(todos_arg,filepath=FILEPATH):
     with open(filepath,'w') as file:
         file.writelines(todos_arg)
         # file=open('files/todos.txt', 'w')
--- gui.py
+++ gui.py
@@ -1,9 +1,32 @@
 import  functions
 import FreeSimpleGUI as sg
 
+
+
+#
+# from cli import new_todo
+
 label=sg.Text("Type in a todo")
-input_box=sg.InputText(tooltip="Enter a todo")
+input_box=sg.InputText(tooltip="Enter a todo",key="todo")
 add_button=sg.Button("Add")
-window=sg.Window("My Todo App",layout=[[label],[input_box,add_button]])
-window.read()
+# listbox=sg.Listbox(functions.get_todos())
+# edit_button=sg.Button("Edit")
+
+window=sg.Window("My Todo App",
+                 layout=[[label],[input_box,add_button]],
+                 font=('Helvetica',20))
+
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
